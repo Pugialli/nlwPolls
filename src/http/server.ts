b778c90ef152a468266c9ebdd9dc5f38ad260247
Pugialli <joaopugialli@gmail.com>
2024-02-06 19:07:16 -0300

    Main

---
+++ src/http/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+    return 'Hello NLW'
+})
+
+
+
+app.listen({port: 3333}).then(()=> {
+    console.log('HTTP server running!')
+})
+
