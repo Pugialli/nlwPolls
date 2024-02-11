0e568a6bc9c1610b9dc0448f573747246afbfe1e
Pugialli <joaopugialli@gmail.com>
2024-02-11 15:49:42 -0300

    projeto finalizado

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import z from "zod"
 import { randomUUID } from "node:crypto"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
+import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -35,6 +37,14 @@ export async function voteOnPoll(app: FastifyInstance) {
                     }
                 })
 
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
+                })
+        
+
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: 'You already voted on this poll' })
             }
@@ -57,6 +67,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 pollId,
                 pollOptionId,
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
         })
 
         return reply.status(201).send({ message: 'Thanks for your vote' })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 
@@ -12,9 +14,12 @@ app.register(cookie, {
     parseOptions: {}
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
